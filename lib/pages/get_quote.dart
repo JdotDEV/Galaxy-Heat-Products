d514fe6fa3c6897701ddd9f04e93b63ae86b44d3
Jhems <Er.jhems@gmail.com>
2023-01-28 21:22:05 +0530

    Copyright text added

--- lib/pages/get_quote.dart
+++ lib/pages/get_quote.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class Get_quote extends StatefulWidget {
@@ -46,7 +47,31 @@ class _Get_quoteState extends State<Get_quote> {
         backgroundColor: Colors.grey[900],
         title: const Text("Enquiry Form"),
       ),
-      body: WebViewWidget(controller: _controller!),
+      body: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 40),
+            child: WebViewWidget(controller: _controller!),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Text(
+                "Copyright © 2023 WebNX. All Rights Reserved.",
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    color: Color.fromARGB(145, 0, 0, 0),
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          
+        ],
+      ),
     );
   }
 }
